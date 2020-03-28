e7d8ecebb140bd075d20fc34f1ed878d61fe28d8
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-03-28 18:27:04 +0530

    creating object of MyBinarySearchTree and taking user input of no of nodes and traversing according to that

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,22 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MyBinarySearchTree bst = new MyBinarySearchTree();
+        System.out.println("enter teh no of nodes");
+        int n = sc.nextInt();
+        while (n-- > 0) {
+
+        }
+    }
+
 }
