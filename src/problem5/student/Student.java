6f627c618d05e1df678746c5ba6acf9d03a18ee9
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-03-28 18:41:20 +0530

    creating getter and setter for three variables

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -9,6 +9,31 @@ package problem5.student;
 public class Student {
     private String name;
     private int rollNumber;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public int getBackLogCounter() {
+        return backLogCounter;
+    }
+
+    public void setBackLogCounter(int backLogCounter) {
+        this.backLogCounter = backLogCounter;
+    }
+
     private int backLogCounter;
 }
 
