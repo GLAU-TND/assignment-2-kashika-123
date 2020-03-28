bc2785feb270a138bc6f790740060a922275ffbb
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-03-28 19:27:32 +0530

    creating getter and setter for node variable front

--- src/problem3/myqueue/MyPriorityQueue.java
+++ src/problem3/myqueue/MyPriorityQueue.java
@@ -6,5 +6,16 @@
  */
 package problem3.myqueue;
 
+import problem5.node.Node;
+
 public class MyPriorityQueue {
+    private Node front;
+
+    public Node getFront() {
+        return front;
+    }
+
+    public void setFront(Node front) {
+        this.front = front;
+    }
 }
