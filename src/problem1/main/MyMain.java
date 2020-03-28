c1dc262ee4e8660e3f0d03db039c4ac35af3f8b7
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-03-28 18:12:15 +0530

    creating object of MyBinarySearchTree and taking no of node from user as input

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -7,5 +7,20 @@
 package problem1.main;
 // executable class
 
+import problem1.mybst.MyBinarySearchTree;
+
+import java.util.Scanner;
+
 public class MyMain {
+    public static void main(String[] args) {
+
+
+        Scanner sc = new Scanner(System.in);
+        MyBinarySearchTree bst = new MyBinarySearchTree();
+        System.out.println("Enter the number of nodes");
+        int n = sc.nextInt();
+        while (n-- > 0) {
+
+        }
+    }
 }
