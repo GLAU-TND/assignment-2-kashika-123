745cc4df8d9ca953b1d4254c832911eda90a443b
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-03-28 20:29:55 +0530

    inserting data in the object of MyBinarySearchTree

--- src/problem4/main/MyMain.java
+++ src/problem4/main/MyMain.java
@@ -5,7 +5,29 @@
  *  Time: 7:08 PM
  */
 package problem4.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+import problem4.myqueue.MyQueue;
+
+import java.util.Scanner;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MyBinarySearchTree bst = new MyBinarySearchTree();
+        MyQueue queue = new MyQueue();
+        System.out.println("enter the number of nodes");
+        int numberOfNodes = sc.nextInt();
+        while (numberOfNodes-- > 0) {
+            System.out.println("enter data");
+            int data = sc.nextInt();
+            TreeNode treeNode = new TreeNode();
+            treeNode.setData(data);
+            bst.insert(treeNode);
+        }
+
+    }
 }
